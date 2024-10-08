fd5b5ab463f5d2044dde6e029a2bb06c8360411c
Justus Klein <j.klein@basecom.de>
2024-10-08 15:58:31 +0200

    feat: test with separate DB_POSTGRES_CONNECTION_STRING
    Signed-off-by: Justus Klein <j.klein@basecom.de>

--- db-drivers/postgres.ts
+++ db-drivers/postgres.ts
@@ -6,15 +6,15 @@ import { defineDatabaseDriver } from '~/lib/db/driver'
 
 export const postgresDriver = defineDatabaseDriver({
   envSchema: z.object({
-    DB_POSTGRES_SSL: z.enum(['true', 'false']).transform((v) => v === 'true'),
-    DB_POSTGRES_DATABASE: z.string(),
-    DB_POSTGRES_HOST: z.string(),
-    DB_POSTGRES_USER: z.string(),
-    DB_POSTGRES_PASSWORD: z.string(),
-    DB_POSTGRES_PORT: z.coerce.number().int(),
+    DB_POSTGRES_CONNECTION_STRING: z.string().nullable(),
+    DB_POSTGRES_DATABASE: z.string().nullable(),
+    DB_POSTGRES_HOST: z.string().nullable(),
+    DB_POSTGRES_USER: z.string().nullable(),
+    DB_POSTGRES_PASSWORD: z.string().nullable(),
+    DB_POSTGRES_PORT: z.coerce.number().int().nullable(),
   }),
   async setup({
-    DB_POSTGRES_SSL,
+    DB_POSTGRES_CONNECTION_STRING,
     DB_POSTGRES_DATABASE,
     DB_POSTGRES_HOST,
     DB_POSTGRES_PASSWORD,
@@ -22,12 +22,12 @@ export const postgresDriver = defineDatabaseDriver({
     DB_POSTGRES_USER,
   }) {
     const pool = new pg.Pool({
-      database: DB_POSTGRES_DATABASE,
-      host: DB_POSTGRES_HOST,
-      password: DB_POSTGRES_PASSWORD,
-      port: DB_POSTGRES_PORT,
-      user: DB_POSTGRES_USER,
-      ssl: DB_POSTGRES_SSL,
+      connectionString: DB_POSTGRES_CONNECTION_STRING || undefined,
+      database: DB_POSTGRES_DATABASE || undefined,
+      host: DB_POSTGRES_HOST || undefined,
+      password: DB_POSTGRES_PASSWORD || undefined,
+      port: DB_POSTGRES_PORT || undefined,
+      user: DB_POSTGRES_USER || undefined,
       max: 10,
     })
     await pool.connect()
